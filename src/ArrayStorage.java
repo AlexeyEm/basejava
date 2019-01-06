5c37cd839392d2a526e88560526f86e97faf89be
Эм Алексей Юрьевич <aem@at-consulting.ru>
2019-01-06 16:22:25 +0600

    Home Work 1

--- src/ArrayStorage.java
+++ src/ArrayStorage.java
@@ -1,30 +1,69 @@
 /**
  * Array based storage for Resumes
  */
+
 public class ArrayStorage {
-    Resume[] storage = new Resume[10000];
+    private Resume[] storage = new Resume[10000];
+    private int size = -1; //Размер массива без null
 
     void clear() {
-    }
-
-    void save(Resume r) {
+        for (int i = 0; i < size + 1; i++) {
+            storage[i] = null;
+        }
+        size = -1;
     }
 
     Resume get(String uuid) {
-        return null;
+        Resume resume = null;
+        for (int i = 0; i < size + 1; i++) {
+            if (storage[i].uuid.equalsIgnoreCase(uuid))
+                resume = storage[i];
+        }
+        return resume;
+    }
+
+    void save(Resume r) {
+        Resume resume = this.get(r.uuid);
+        //Если резюме нет в массиве, то создаем
+        if (resume == null) {
+            size++;
+            storage[size] = r;
+        }
     }
 
     void delete(String uuid) {
+        boolean isNeedShift = false; //Признак: нужен сдвиг
+
+        for (int i = 0; i < size + 1; i++) {
+            //Если удаляется непоследний элемент массива
+            if (storage[i].uuid.equalsIgnoreCase(uuid) && i != size) {
+                isNeedShift = true;
+                size--;
+            }
+            //Если удаляется последний элемент массива
+            if (storage[i].uuid.equalsIgnoreCase(uuid) && i == size) {
+                storage[i] = null;
+                size--;
+            }
+            //Сдвиг влево
+            if (isNeedShift) {
+                storage[i] = storage[i + 1];
+            }
+        }
     }
 
     /**
      * @return array, contains only Resumes in storage (without null)
      */
     Resume[] getAll() {
-        return new Resume[0];
+        Resume[] resultStorage = new Resume[size + 1];
+        if (size >= 0) {
+            System.arraycopy(storage, 0, resultStorage, 0, size + 1);
+        }
+        return resultStorage;
     }
 
     int size() {
-        return 0;
+        return this.size + 1;
     }
 }
