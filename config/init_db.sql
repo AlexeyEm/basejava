77317fd52614eeee7eb02df4d33e4de2735cbb68
Эм Алексей Юрьевич <aem@at-consulting.ru>
2019-04-06 08:00:40 +0600

    HW15: init_db.sql: fixed

--- config/init_db.sql
+++ config/init_db.sql
@@ -1,18 +1,26 @@
-CREATE TABLE resume
+create table resume
 (
-  uuid      varchar(36) PRIMARY KEY NOT NULL,
-  full_name TEXT                 NOT NULL
+  uuid      varchar(36) not null
+    constraint resume_pk
+      primary key,
+  full_name text        not null
 );
 
-CREATE TABLE contact
+create table contact
 (
-  id          SERIAL,
-  resume_uuid varchar(36) NOT NULL REFERENCES resume (uuid) ON DELETE CASCADE,
-  type        TEXT     NOT NULL,
-  value       TEXT     NOT NULL
+  id          serial      not null
+    constraint contact_pk
+      primary key,
+  type        text        not null,
+  value       text        not null,
+  resume_uuid varchar(36) not null
+    constraint contact_resume_uuid_fk
+      references resume
+      on delete cascade
 );
-CREATE UNIQUE INDEX contact_uuid_type_index
-  ON contact (resume_uuid, type);
+
+create unique index contact_uuid_type_index
+  on contact (resume_uuid, type);
 
 create table section
 (
